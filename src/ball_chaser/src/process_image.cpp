d904002410f15717c7ad034bd205b9a6a17913e5
root <alaaedin.mohamad@gmail.com>
2020-12-06 19:59:46 +0000

    code implementation is done

--- src/ball_chaser/src/process_image.cpp
+++ src/ball_chaser/src/process_image.cpp
@@ -4,23 +4,68 @@
 
 // Define a global client that can request services
 ros::ServiceClient client;
+bool isMoving = false;
 
 // This function calls the command_robot service to drive the robot in the specified direction
 void drive_robot(float lin_x, float ang_z)
 {
     // TODO: Request a service and pass the velocities to it to drive the robot
+    ROS_INFO_STREAM("Driving the robot...");
+
+    ball_chaser::DriveToTarget drivingSvc;
+    drivingSvc.request.linear_x = lin_x;
+    drivingSvc.request.angular_z = ang_z;
+
+    if(!client.call(drivingSvc))
+    {
+        ROS_ERROR("Failed calling [DriveToTarget] service");
+    }
 }
 
 // This callback function continuously executes and reads the image data
 void process_image_callback(const sensor_msgs::Image img)
 {
-
+    bool isBallFound = false;
     int white_pixel = 255;
-
+    int midSectionBegin = img.width/3;
+    int midSectionEnd = 2 * (img.width/3);
     // TODO: Loop through each pixel in the image and check if there's a bright white one
     // Then, identify if this pixel falls in the left, mid, or right side of the image
     // Depending on the white ball position, call the drive_bot function and pass velocities to it
     // Request a stop when there's no white ball seen by the camera
+    
+    for(int i = 0; i < img.height * img.step; i++)
+    {
+        if(img.data[i] == white_pixel)
+        {
+            ROS_INFO("Ball is found...");
+            int col = i / img.step;
+            isBallFound = true;
+            if(col <= midSectionBegin)
+            {
+                drive_robot(0.2, -0.2);
+                ROS_INFO("Driving left");
+                break;
+            }
+            if(col > midSectionBegin && col <= midSectionEnd )
+            {
+                drive_robot(0.2, 0);
+                ROS_INFO("Driving forward");
+                break;
+            }
+            if(col > midSectionEnd)
+            {
+                drive_robot(0.2, 0.2);
+                ROS_INFO("Driving right");
+                break;
+            }
+        }
+        if(!isBallFound)
+        {
+            ROS_INFO("Ball could not be found...");
+            drive_robot(0, 0);
+        }
+    }
 }
 
 int main(int argc, char** argv)
